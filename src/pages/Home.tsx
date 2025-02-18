ed87e82b8da9ebe3fa47f42174d27fdc205ed524
pekingzcc <pekingzcc@gmail.com>
2025-02-18 12:34:08 +0800

    change game

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -78,7 +78,7 @@ function Home() {
       image: "/drift-king.webp",
       features: ["Drift Mechanics", "Car Tuning", "Global Leaderboards"],
       isNew: false,
-      iframe: '<iframe src="https://www.gameflare.com/embed/up-hill-racing-2" style="width: 100%; height: 100%;" frameborder="0" allow="gamepad *;"></iframe>'
+      iframe: '<iframe src="https://www.gameflare.com/embed/dream-car-racing-2/" style="width: 100%; height: 100%;" frameborder="0" allow="gamepad *;"></iframe>'
     },
     {
       id: 5,
@@ -87,7 +87,7 @@ function Home() {
       image: "/rally-champion.webp",
       features: ["Weather System", "Track Editor", "Championship Mode"],
       isNew: false,
-      iframe: '<iframe src="https://www.gameflare.com/embed/gun-racing/" style="width: 100%; height: 100%;" frameborder="0" allow="gamepad *;"></iframe>'
+      iframe: '<iframe src="https://www.gameflare.com/embed/uphill-climb-racing-3/" style="width: 100%; height: 100%;" frameborder="0" allow="gamepad *;"></iframe>'
     },
     {
       id: 6,
